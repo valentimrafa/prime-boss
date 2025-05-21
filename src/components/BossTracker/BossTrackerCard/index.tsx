5a50ca0da3ec4defe6beb8bf5bd498689d097fe6
Rafael Valentim <rafael.valentim@outlook.com.br>
2025-05-21 20:47:56 -0300

    fix nos status do card de boss

--- src/components/BossTracker/BossTrackerCard/index.tsx
+++ src/components/BossTracker/BossTrackerCard/index.tsx
@@ -65,44 +65,44 @@ export function BossTrackerCard({ boss, actions = [] }: BossTrackerCardProps) {
 
   const borderColor =
     boss.status === "MORTO" ? "border-red-500" : "border-green-500";
-  const borderColorStatus =
+  const borderCardColorStatus =
     timeToRebirthText !== "" ? borderColor : "border-yellow-500";
-  const bossStatusColor =
-    boss.status === "MORTO"
-      ? "bg-red-100 text-red-800"
-      : "bg-yellow-100 text-yellow-800";
 
   const blockButtonClock =
     boss.status === "MORTO" && waitingDateTimeHigherThanNow;
   return (
     <div
-      className={`grow w-full bg-white rounded-2xl shadow-md border-l-6 transition-colors ${borderColorStatus} uppercase`}
+      className={`grow w-full bg-white rounded-2xl shadow-md border-l-6 transition-colors ${borderCardColorStatus} uppercase`}
     >
       <div className="flex flex-col md:flex-row">
-        <div className="flex-1 p-4 border-b md:border-b-0 md:border-r border-gray-200">
+        <div className="flex-1 p-4 border-b md:border-b-0 md:border-r border-gray-200 flex flex-col gap-2">
           <h2 className="text-xl font-bold text-blue-500">
             {boss.boss?.name} - {boss.server?.name} ({boss.server?.type})
           </h2>
           <p className="text-black">Nascimento: {formatedDateTimeToRebirth}</p>
 
-          <p className={`font-bold`}>
+          <p className="font-bold">
             {boss.status === "PENDENTE" && isFuture && (
-              <span className="text-green-500">{timeToRebirthText}</span>
+              <span className="bg-green-100 px-4 py-2 rounded-sm">
+                {timeToRebirthText}
+              </span>
             )}
 
             {boss.status === "PENDENTE" && !isFuture && (
-              <span className="bg-red-500">BOSS ATRASADO</span>
-            )}
-            {boss.status === "MORTO" && (
-              <span className="bg-yellow-500">
-                VAI SER LIBERADO DAQUI ${formattedNextLiberationTime}
+              <span className="bg-yellow-100 px-4 py-2 rounded-sm">
+                BOSS ATRASADO
               </span>
             )}
-          </p>
-          <p
-            className={`mt-2 inline-block px-2 py-1 font-semibold rounded ${bossStatusColor}`}
-          >
-            Status: {boss.status}
+            {boss.status === "MORTO" && waitingDateTimeHigherThanNow && (
+              <span className="bg-red-100 px-4 py-2 rounded-sm">
+                VAI SER LIBERADO DAQUI {formattedNextLiberationTime}
+              </span>
+            )}
+            {boss.status === "MORTO" && !waitingDateTimeHigherThanNow && (
+              <span className="bg-red-100 px-4 py-2 rounded-sm">
+                BOSS AGUARDANDO NOVO HORÁRIO
+              </span>
+            )}
           </p>
         </div>
         <div className="flex gap-2 flex-wrap p-4 justify-center items-start md:items-center md:flex-col">
