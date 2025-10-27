5fdd2ba6ad0e59f2104102e29bdb7eeb40e790fb
Rafael Valentim <rafael.valentim@outlook.com.br>
2025-10-27 13:46:35 -0300

    ajustada hora

--- src/services/bossTracker.service.ts
+++ src/services/bossTracker.service.ts
@@ -93,7 +93,7 @@ class BossTrackerService {
           idServer: server.id,
           idBoss: boss.id,
           status: "PENDENTE",
-          rebirth: calculateNextBossDateTime("04:35"),
+          rebirth: calculateNextBossDateTime("04:10"),
         });
       }
     }
